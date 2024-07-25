9c011622a9fdfb308d6554f7731f71473ab2d2c7
Dmitrynest2012 <53977888+Dmitrynest2012@users.noreply.github.com>
2024-07-25 10:37:47 +0300

    Update script.js
    Добавлен функционал переключения дня и сохранения приемов пищи.

--- script.js
+++ script.js
@@ -2,13 +2,58 @@ document.addEventListener("DOMContentLoaded", function() {
     const productsDiv = document.getElementById("products");
     const searchInput = document.getElementById("search-input");
     const suggestionsDiv = document.getElementById("suggestions");
+    const currentDayInput = document.getElementById("current-day");
+    const prevDayButton = document.getElementById("prev-day");
+    const nextDayButton = document.getElementById("next-day");
 
     let products = [];
-    let mealTotals = {
+    let selectedDate = new Date();
+    let mealData = {};
+
+    const mealTotals = {
         breakfast: { calories: 0, protein: 0, carbs: 0, fats: 0, fiber: 0, water: 0, grams: 0 },
         lunch: { calories: 0, protein: 0, carbs: 0, fats: 0, fiber: 0, water: 0, grams: 0 },
         dinner: { calories: 0, protein: 0, carbs: 0, fats: 0, fiber: 0, water: 0, grams: 0 }
     };
+
+    function formatDate(date) {
+        return date.toISOString().split('T')[0];
+    }
+
+    function updateDateDisplay() {
+        currentDayInput.value = selectedDate.toLocaleDateString('ru-RU', { year: 'numeric', month: 'long', day: 'numeric' });
+    }
+
+    function loadMealData() {
+        const dateKey = formatDate(selectedDate);
+        const storedData = JSON.parse(localStorage.getItem(dateKey)) || { breakfast: [], lunch: [], dinner: [] };
+        mealData = storedData;
+
+        // Обновляем отображение приемов пищи и итогов
+        ["breakfast", "lunch", "dinner"].forEach(meal => {
+            document.getElementById(`${meal}-products`).innerHTML = '';
+            mealTotals[meal] = { calories: 0, protein: 0, carbs: 0, fats: 0, fiber: 0, water: 0, grams: 0 };
+            storedData[meal].forEach(product => {
+                updateMealSummary(meal, product);
+            });
+            updateMealTotal(meal);
+        });
+        updateDailySummary();
+    }
+
+    function saveMealData() {
+        const dateKey = formatDate(selectedDate);
+        localStorage.setItem(dateKey, JSON.stringify(mealData));
+    }
+
+    function changeDay(offset) {
+        selectedDate.setDate(selectedDate.getDate() + offset);
+        updateDateDisplay();
+        loadMealData();
+    }
+
+    prevDayButton.addEventListener('click', () => changeDay(-1));
+    nextDayButton.addEventListener('click', () => changeDay(1));
 
     // Загрузка продуктов из JSON файла
     fetch('products.json')
@@ -18,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function() {
             searchInput.addEventListener('input', handleSearchInput);
         });
 
-    // Обработка ввода в поисковое поле
+     // Обработка ввода в поисковое поле
     function handleSearchInput() {
         const query = searchInput.value.toLowerCase();
         suggestionsDiv.innerHTML = '';
@@ -44,7 +89,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    // Отображение карточки продукта
+     // Отображение карточки продукта
     function displayProductCard(product) {
         productsDiv.innerHTML = '';
 
@@ -68,6 +113,7 @@ document.addEventListener("DOMContentLoaded", function() {
             <img src="${product.image}" alt="${product.name}">
             <div>
                 <h3>${product.name}</h3>
+                ${product.description ? `<p>${product.description}</p>` : ''}
                 <label>Введите вес продукта (граммы):
                     <input type="number" class="product-weight" min="1" step="1" value="${defaultWeight}">
                 </label>
@@ -80,7 +126,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 <select class="serving-size">
                     ${updateServingsOptions(defaultWeight)}
                 </select>
-                <input type="number" class="serving-amount" placeholder="Количество дозировок" min="1" step="1">
+                <input type="number" class="serving-amount" placeholder="Количество порций" min="1" step="1">
                 <select class="meal-time">
                     <option value="">Выберите прием пищи</option>
                     ${mealsOptions}
@@ -115,7 +161,7 @@ document.addEventListener("DOMContentLoaded", function() {
             servingSizeSelect.innerHTML = updateServingsOptions(weight);
         }
 
-        updateNutritionalInfo(); // Первоначальное обновление информации
+        updateNutritionalInfo();  // Первоначальное обновление информации
 
         const addToMealButton = card.querySelector(".add-to-meal");
         addToMealButton.addEventListener("click", function() {
@@ -147,128 +193,152 @@ document.addEventListener("DOMContentLoaded", function() {
             const waterPerServing = (product.waterContent * weight) / 100;
             const fiberPerServing = (product.fiberContent * weight) / 100;
 
-            // Обновление сводной информации по приему пищи
-            function updateMealSummary(meal, calories, protein, carbs, fats, fiber, water, productName, servingSize, servingAmount) {
-                const mealProductsDiv = document.getElementById(`${meal}-products`);
-                const mealTotalEl = document.getElementById(`${meal}-total`);
-
-                // Проверяем, нужно ли добавить заголовок таблицы
-                if (mealProductsDiv.innerHTML.trim() === '') {
-                    mealProductsDiv.innerHTML = `
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Продукт</th>
-                                    <th>Количество</th>
-                                    <th>Калории</th>
-                                    <th>Белки</th>
-                                    <th>Жиры</th>
-                                    <th>Углеводы</th>
-                                    <th>Клетчатка</th>
-                                    <th>Вода</th>
-                                    <th>Управление</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            </tbody>
-                        </table>
-                    `;
-                }
-
-                // Добавляем запись о продукте в таблицу
-                const tableBody = mealProductsDiv.querySelector('tbody');
-                const productEntry = document.createElement("tr");
-                productEntry.classList.add("draggable");
-                productEntry.draggable = true;
-                productEntry.innerHTML = `
-                    <td>${productName}</td>
-                    <td>${servingAmount} ${servingSize} [${weight.toFixed(2)} г]</td>
-                    <td>${caloriesPerServing.toFixed(2)} ккал</td>
-                    <td>${proteinPerServing.toFixed(2)} г</td>
-                    <td>${fatsPerServing.toFixed(2)} г</td>
-                    <td>${carbsPerServing.toFixed(2)} г</td>
-                    <td>${fiberPerServing.toFixed(2)} г</td>
-                    <td>${waterPerServing.toFixed(2)} мл</td>
-                    <td><span class="remove-btn">X</span></td>
-                `;
-                tableBody.appendChild(productEntry);
-
-                // Добавляем обработчик для удаления продукта
-                productEntry.querySelector('.remove-btn').addEventListener('click', () => {
-                    tableBody.removeChild(productEntry);
-                    mealTotals[meal].calories -= calories;
-                    mealTotals[meal].protein -= protein;
-                    mealTotals[meal].carbs -= carbs;
-                    mealTotals[meal].fats -= fats;
-                    mealTotals[meal].fiber -= fiber;
-                    mealTotals[meal].water -= water;
-                    mealTotals[meal].grams -= weight;
-                    updateMealTotal(meal);
-                    updateDailySummary();
-                });
-
-                // Добавляем обработчики для перетаскивания
-                productEntry.addEventListener('dragstart', (e) => {
-                    e.dataTransfer.setData('text/plain', productEntry.rowIndex);
-                });
-
-                productEntry.addEventListener('dragover', (e) => {
-                    e.preventDefault();
-                });
-
-                productEntry.addEventListener('drop', (e) => {
-                    e.preventDefault();
-                    const draggedRowIndex = e.dataTransfer.getData('text/plain');
-                    const draggedRow = tableBody.rows[draggedRowIndex];
-                    if (draggedRow !== productEntry) {
-                        const temp = document.createElement('tr');
-                        tableBody.insertBefore(temp, productEntry);
-                        tableBody.insertBefore(productEntry, draggedRow);
-                        tableBody.insertBefore(draggedRow, temp);
-                        tableBody.removeChild(temp);
-                    }
-                });
-
-                mealTotals[meal].calories += calories;
-                mealTotals[meal].protein += protein;
-                mealTotals[meal].carbs += carbs;
-                mealTotals[meal].fats += fats;
-                mealTotals[meal].fiber += fiber;
-                mealTotals[meal].water += water;
-                mealTotals[meal].grams += weight;
-
-                updateMealTotal(meal);
-                updateDailySummary(); // Обновляем общие итоги за день
+            const productEntry = {
+                name: productName,
+                weight: weight,
+                servingSize: servingSize,
+                servingAmount: servingAmount,
+                calories: caloriesPerServing,
+                protein: proteinPerServing,
+                carbs: carbsPerServing,
+                fats: fatsPerServing,
+                fiber: fiberPerServing,
+                water: waterPerServing
             };
 
-            // Обновление итоговой строки для приема пищи
-            function updateMealTotal(meal) {
-                const mealTotalEl = document.getElementById(`${meal}-total`);
-                mealTotalEl.innerHTML = `
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Итог:</th>
-                                <th>${mealTotals[meal].grams.toFixed(2)} г</th>
-                                <th>${mealTotals[meal].calories.toFixed(2)} ккал</th>
-                                <th>${mealTotals[meal].protein.toFixed(2)} г</th>
-                                <th>${mealTotals[meal].fats.toFixed(2)} г</th>
-                                <th>${mealTotals[meal].carbs.toFixed(2)} г</th>
-                                <th>${mealTotals[meal].fiber.toFixed(2)} г</th>
-                                <th>${mealTotals[meal].water.toFixed(2)} мл</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                    </table>
-                `;
+            if (!mealData[mealTime]) {
+                mealData[mealTime] = [];
             }
+            mealData[mealTime].push(productEntry);
 
-            // Обновляем сводную информацию по приему пищи
-            updateMealSummary(mealTime, caloriesPerServing, proteinPerServing, carbsPerServing, fatsPerServing, fiberPerServing, waterPerServing, productName, servingSize, servingAmount);
+            updateMealSummary(mealTime, productEntry);
+            updateMealTotal(mealTime);
+            updateDailySummary();
+            saveMealData();
         });
     }
 
-    // Обновление общей информации за день
+     // Обновление сводной информации по приему пищи
+    function updateMealSummary(meal, productEntry) {
+        const mealProductsDiv = document.getElementById(`${meal}-products`);
+        const mealTotalEl = document.getElementById(`${meal}-total`);
+
+        // Проверяем, нужно ли добавить заголовок таблицы
+        if (mealProductsDiv.innerHTML.trim() === '') {
+            mealProductsDiv.innerHTML = `
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Продукт</th>
+                            <th>Количество</th>
+                            <th>Калории</th>
+                            <th>Белки</th>
+                            <th>Жиры</th>
+                            <th>Углеводы</th>
+                            <th>Клетчатка</th>
+                            <th>Вода</th>
+                            <th>Управление</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            `;
+        }
+
+        // Добавляем запись о продукте в таблицу
+        const tableBody = mealProductsDiv.querySelector('tbody');
+        const productRow = document.createElement("tr");
+        productRow.classList.add("draggable");
+        productRow.draggable = true;
+        productRow.innerHTML = `
+            <td>${productEntry.name}</td>
+            <td>${productEntry.servingAmount} ${productEntry.servingSize} [${productEntry.weight.toFixed(2)} г]</td>
+            <td>${productEntry.calories.toFixed(2)} ккал</td>
+            <td>${productEntry.protein.toFixed(2)} г</td>
+            <td>${productEntry.fats.toFixed(2)} г</td>
+            <td>${productEntry.carbs.toFixed(2)} г</td>
+            <td>${productEntry.fiber.toFixed(2)} г</td>
+            <td>${productEntry.water.toFixed(2)} мл</td>
+            <td><span class="remove-btn">X</span></td>
+        `;
+        tableBody.appendChild(productRow);
+
+        // Добавляем обработчик для удаления продукта
+        productRow.querySelector('.remove-btn').addEventListener('click', () => {
+            tableBody.removeChild(productRow);
+            const index = mealData[meal].indexOf(productEntry);
+            if (index > -1) {
+                mealData[meal].splice(index, 1);
+            }
+            mealTotals[meal].calories -= productEntry.calories;
+            mealTotals[meal].protein -= productEntry.protein;
+            mealTotals[meal].carbs -= productEntry.carbs;
+            mealTotals[meal].fats -= productEntry.fats;
+            mealTotals[meal].fiber -= productEntry.fiber;
+            mealTotals[meal].water -= productEntry.water;
+            mealTotals[meal].grams -= productEntry.weight;
+            updateMealTotal(meal);
+            updateDailySummary();
+            saveMealData();
+        });
+
+        // Добавляем обработчики для перетаскивания
+        productRow.addEventListener('dragstart', (e) => {
+            e.dataTransfer.setData('text/plain', productRow.rowIndex);
+        });
+
+        productRow.addEventListener('dragover', (e) => {
+            e.preventDefault();
+        });
+
+        productRow.addEventListener('drop', (e) => {
+            e.preventDefault();
+            const draggedRowIndex = e.dataTransfer.getData('text/plain');
+            const draggedRow = tableBody.rows[draggedRowIndex];
+            if (draggedRow !== productRow) {
+                const temp = document.createElement('tr');
+                tableBody.insertBefore(temp, productRow);
+                tableBody.insertBefore(productRow, draggedRow);
+                tableBody.insertBefore(draggedRow, temp);
+                tableBody.removeChild(temp);
+            }
+        });
+
+        mealTotals[meal].calories += productEntry.calories;
+        mealTotals[meal].protein += productEntry.protein;
+        mealTotals[meal].carbs += productEntry.carbs;
+        mealTotals[meal].fats += productEntry.fats;
+        mealTotals[meal].fiber += productEntry.fiber;
+        mealTotals[meal].water += productEntry.water;
+        mealTotals[meal].grams += productEntry.weight;
+
+        updateMealTotal(meal);
+    }
+
+    // Обновление итоговой строки для приема пищи
+    function updateMealTotal(meal) {
+        const mealTotalEl = document.getElementById(`${meal}-total`);
+        mealTotalEl.innerHTML = `
+            <table>
+                <thead>
+                    <tr>
+                        <th>Итог:</th>
+                        <th>${mealTotals[meal].grams.toFixed(2)} г</th>
+                        <th>${mealTotals[meal].calories.toFixed(2)} ккал</th>
+                        <th>${mealTotals[meal].protein.toFixed(2)} г</th>
+                        <th>${mealTotals[meal].fats.toFixed(2)} г</th>
+                        <th>${mealTotals[meal].carbs.toFixed(2)} г</th>
+                        <th>${mealTotals[meal].fiber.toFixed(2)} г</th>
+                        <th>${mealTotals[meal].water.toFixed(2)} мл</th>
+                        <th></th>
+                    </tr>
+                </thead>
+            </table>
+        `;
+    }
+
+     // Обновление общей информации за день
     function updateDailySummary() {
         const dailyCaloriesEl = document.getElementById("daily-calories");
         const dailyProteinEl = document.getElementById("daily-protein");
@@ -277,7 +347,6 @@ document.addEventListener("DOMContentLoaded", function() {
         const dailyWaterEl = document.getElementById("daily-water");
         const dailyFiberEl = document.getElementById("daily-fiber");
 
-        // Считаем общие калории, белки, углеводы, жиры, клетчатку и воду за день
         const totalCalories = Object.values(mealTotals).reduce((sum, meal) => sum + meal.calories, 0);
         const totalProtein = Object.values(mealTotals).reduce((sum, meal) => sum + meal.protein, 0);
         const totalCarbs = Object.values(mealTotals).reduce((sum, meal) => sum + meal.carbs, 0);
@@ -285,7 +354,6 @@ document.addEventListener("DOMContentLoaded", function() {
         const totalFiber = Object.values(mealTotals).reduce((sum, meal) => sum + meal.fiber, 0);
         const totalWater = Object.values(mealTotals).reduce((sum, meal) => sum + meal.water, 0);
 
-        // Отображаем общие значения за день
         dailyCaloriesEl.textContent = totalCalories.toFixed(2);
         dailyProteinEl.textContent = totalProtein.toFixed(2);
         dailyCarbsEl.textContent = totalCarbs.toFixed(2);
@@ -293,4 +361,8 @@ document.addEventListener("DOMContentLoaded", function() {
         dailyFiberEl.textContent = totalFiber.toFixed(2);
         dailyWaterEl.textContent = totalWater.toFixed(2);
     }
+
+    // Инициализация текущей даты
+    updateDateDisplay();
+    loadMealData();
 });
